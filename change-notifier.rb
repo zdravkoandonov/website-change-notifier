4badcc57aa5f7d826e51b7ceb7fa1c2e197bd1f6
Zdravko Andonov <zdravko.andonov.dev@gmail.com>
2015-12-04 15:27:22 +0200

    Added update_all function to update all set urls

--- change-notifier.rb
+++ change-notifier.rb
@@ -4,24 +4,45 @@ require 'net/smtp'
 class ChangeNotifier
   attr_reader :last_change_line
 
-  def initialize(url)
-    @url = url
-
+  def initialize(*urls)
+    @urls = urls
   end
 
-  def dl
+  def update_all
+    @urls.each do |url|
+      dl(url)
+      last_change_line = diff_last_two_txts(url)
+      p last_change_line
+      if last_change_line
+        send_email(url, last_change_line + 1)
+      end
+    end
+  end
+
+  def dl(url)
     now = Time.now
-    response = Net::HTTP.get_response(URI(@url))
-    File.write("dl#{@url.gsub(/http:|\/|\\|\./, '')}#{now.strftime('%Y%m%d%H%M%S%L')}", response.body)
+    response = Net::HTTP.get_response(URI(url))
+
+    File.write("#{url.gsub(/http:|\/|\\|\./, '')}#{now.strftime('%Y%m%d%H%M%S%L')}", response.body)
   end
 
-  def send_email
+  def diff_last_two_txts(url)
+    latest_files = Dir["*"].select { |file_name| file_name.start_with?(url.gsub(/http:|\/|\\|\./, '')) }.sort
+    if latest_files.size >= 2
+      old_file = File.open(latest_files[-2], 'r')
+      new_file = File.open(latest_files[-1], 'r')
+      zipped = new_file.each_line.zip(old_file.each_line)
+      zipped.find_index { |new_line, old_line| new_line != old_line }
+    end
+  end
+
+  def send_email(url, line)
     message = <<MESSAGE_END
 From: zdravkoandonov.com notifier <notifier@zdravkoandonov.com>
 To: Zdravko Andonov <zdravko.andonov.dev@gmail.com>
-Subject: Change detected at #{@url} - #{Time.now}
+Subject: Change detected at #{url} - #{Time.now}
 
-There is a change detected at the following url on line #{@last_change_line+1}: #{@url}
+There is a change detected at the following url on line #{line}: #{url}
 
 MESSAGE_END
 
@@ -34,19 +55,7 @@ MESSAGE_END
       smtp.send_message message, 'notifier@zdravkoandonov.com', 'zdravko.andonov.dev@gmail.com'
     end
   end
-
-  def diff_last_two_txts
-    latest_files = Dir["*"].select { |file_name| file_name.start_with?("dl") }.sort
-    old_file = File.open(latest_files[-2], 'r')
-    new_file = File.open(latest_files[-1], 'r')
-    zipped = new_file.each_line.zip(old_file.each_line)
-    @last_change_line = zipped.find_index { |new_line, old_line| new_line != old_line }
-  end
 end
 
-credoweb_robots_notifier = ChangeNotifier.new("http://www.credoweb.bg/robots.txt")
-credoweb_robots_notifier.dl
-credoweb_robots_notifier.diff_last_two_txts
-if credoweb_robots_notifier.last_change_line
-  credoweb_robots_notifier.send_email
-end
+urls_to_track = ["http://www.credoweb.bg/robots.txt", "http://fmi.ruby.bg/"]
+ChangeNotifier.new(*urls_to_track).update_all
