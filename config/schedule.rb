f8b2be8917b6ad1654d3471a48e1e6ef70e7a6c7
Zdravko Andonov <zdravko.andonov.dev@gmail.com>
2015-12-04 11:12:47 +0200

    Cron task scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,9 +3,16 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
+# use 'whenever -i' to apply
+
 # set :output, "../cron_log.log"
 
-every :day, :at => '17:28 pm' do
-  command :task => "ruby capture.rb"
-  command :task => "ruby diff.rb"
+# every :day, :at => '17:28 pm' do
+#   command :task => "ruby capture.rb"
+#   command :task => "ruby diff.rb"
+# end
+
+every '17 * * * *' do
+  command "cd change-notifier/ && ruby ~/Source/Repos/website-change-notifier/change-notifier.rb"
 end
+
