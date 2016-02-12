8f6e9882e37757306d469b36638ecfb9fe09026f
Zdravko Andonov <zdravko.andonov.dev@gmail.com>
2016-02-12 22:32:47 +0200

    Freeze APP_DIRECTORY

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ require 'yaml'
 db_options = YAML.load(File.read('./config/database.yml'))
 ActiveRecord::Base.establish_connection(db_options)
 
-APP_DIRECTORY = '/home/zdravkoandonov/Source/Repos/website-change-notifier'
+APP_DIRECTORY = '/home/zdravkoandonov/Source/Repos/website-change-notifier'.freeze
