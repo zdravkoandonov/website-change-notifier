6e32a991b3baabd392da18debea7b934226d1768
Zdravko Andonov <zdravko.andonov.dev@gmail.com>
2016-02-13 00:17:11 +0200

    Shorten diff_idnex_of_last_two_files name and remove unnecessary logs

--- workers/differ.rb
+++ workers/differ.rb
@@ -2,9 +2,7 @@ class Differ
   include Sidekiq::Worker
 
   def perform(task_id, original_download_time_string)
-    task = Task.find(task_id)
-
-    diff_line = diff_index_of_last_two_files(task_id, task.selector)
+    diff_line = diff_last_two_files(task_id, Task.find(task_id).selector)
 
     log_message = "#{Time.now} Started file diff for task #{task_id}\n" \
       "Diff line: #{diff_line}"
@@ -12,12 +10,12 @@ class Differ
 
     if diff_line
       log_message = "#{Time.now} There were differences. Sending notification"
-      LogItem.new('diff', log_message).save
-      Notifier.perform_async(task.id, original_download_time_string)
+      Notifier.perform_async(task_id, original_download_time_string)
     else
       log_message = "#{Time.now} No differences - not sending notification"
-      LogItem.new('diff', log_message).save
     end
+
+    LogItem.new('diff', log_message).save
   end
 
   private
@@ -30,11 +28,8 @@ class Differ
     downloaded_files.sort.last(2)
   end
 
-  def diff_index_of_last_two_files(task_id, css_selector = 'html')
+  def diff_last_two_files(task_id, css_selector = 'html')
     old_file_name, new_file_name = latest_two_files(task_id)
-
-    LogItem.new('diff', "Old file name: #{old_file_name}").save
-    LogItem.new('diff', "New file name: #{new_file_name}").save
 
     if old_file_name and new_file_name
       old_file = Nokogiri::HTML(File.open(old_file_name, 'r')).css(css_selector)
